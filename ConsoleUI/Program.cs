187ff068239356dd436ecccdd48c4f481714c766
Ubuntu <ubuntu@ip-172-31-30-163.eu-west-1.compute.internal>
2023-05-26 12:10:31 +0000

    Updated .gitignore

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,19 +5,4 @@ using SharedClasses.Models.CountryModels;
 using SharedClasses.Models.UserModels;
 
 
-
-
-
-
-
-var bank = (Bank)DataBaseClient.BankCollection.Find(Builders<Bank>.Filter.Eq(x => x.CountryOfOrigin, 
-    ((Country)DataBaseClient.CountryCollection.Find(Builders<Country>.Filter.Eq(x => x.Name, "Russia"))).Id
-));
-
-var player = DataBaseClient.PlayerCollection.Find(Builders<Player>.Filter.Eq(x => x.Username, "CUrsache123")).FirstOrDefault();
-
-
-bank.CreateAccount(
-);
-
-
+Console.WriteLine("Hello World");
