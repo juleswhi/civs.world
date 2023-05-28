793ba997d325a7b707fa707f9ba3f681c17bcfe9
Jules White <jules.white1@protonmail.com>
2023-05-28 22:41:51 +0100

    remove references to HomeController.cs

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        var dictCreate = new PopulateMapDictionary();
-        var valuesDictionary = dictCreate.PopulateDictionary();
+        // var dictCreate = new PopulateMapDictionary();
+        // var valuesDictionary = dictCreate.PopulateDictionary();
 
 
         return View();
