fa098e41a8c71c2bbc87894621127e2c30667fe0
Jules White <jules.white1@protonmail.com>
2023-05-29 20:31:01 +0100

    Modified signing.cshtml

--- WebUI/Controllers/SignUpController.cs
+++ WebUI/Controllers/SignUpController.cs
@@ -1,27 +1,7 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using WebUI.Models;
-using SharedClasses.Helpers.CountryMapSerialisation;
-using SharedClasses.Helpers;
-using MongoDB.Driver;
-using SharedClasses.Models.UserModels;
-
-
 namespace WebUI.Controllers;
-
 
 public class SignUpController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult SignUp()
-    {
-        return View();
-    }
- 
     public IActionResult SignIn()
     {
         return View();
