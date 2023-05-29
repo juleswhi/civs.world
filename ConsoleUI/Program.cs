b30e61d4b46c05dbc98325b2d98270f0c98a62ca
Jules White <jules.white1@protonmail.com>
2023-05-29 13:33:39 +0100

    Update Country Codes

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,19 +1,28 @@
 ﻿Console.WriteLine("Hello World");
 
 var country = DataBaseClient.CountryCollection.Find(
-    Builders<Country>.Filter.Eq(x => x.Name, "Ireland")
-).FirstOrDefault();
-
-if (country is null) return;
-
-Console.WriteLine("Country is found");
+    Builders<Country>.Filter.Exists(x => x.Id)
+).ToList();
 
 
+foreach(var i in country)
+{
+    if(i.Name.Contains("Sudan"))
+        Console.WriteLine(i.CountryCode + " " + i.Name);
+    if(i.Name.Contains("Serbia"))
+        Console.WriteLine(i.CountryCode + " " + i.Name);
+    if(i.Name.Contains("Kosovo"))
+        Console.WriteLine(i.CountryCode + " " + i.Name);
+}
 
+var rng = new Random();
 
-await Player.CreatePlayer(
-new Name("Jules", "White"),
-    "Pass6969",
-    "JulesWhi",
-    country.Id
-);
+/*
+
+var filter = Builders<Country>.Filter.Eq(x => x.Name, "Sudan");
+
+var update = Builders<Country>.Update.Set(x => x.CountryCode, "SS");
+
+await DataBaseClient.CountryCollection.UpdateOneAsync(filter, update);
+
+*/
