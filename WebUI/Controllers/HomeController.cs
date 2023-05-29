da659e97883d41fb1eeff310cdffa747c52287de
Jules White <jules.white1@protonmail.com>
2023-05-29 16:33:23 +0100

    Add SignIn

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -32,6 +32,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
