41977833c37f8ea19233dbbb8580f74ed6b8a80a
Jules White <jules.white1@protonmail.com>
2023-05-31 21:56:54 +0100

    Add blank test page

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ public class HomeController : Controller
         return RedirectToAction("Index", "Home");
     }
 
+    public IActionResult Blank()
+    {
+        return View();
+    }
+
 
 
 
