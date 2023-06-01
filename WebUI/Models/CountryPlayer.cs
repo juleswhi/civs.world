5f554ab9fab6b39eae25158afa7f9f54b5499f82
Jules White <jules.white1@protonmail.com>
2023-06-01 19:16:49 +0100

    should work

---
+++ WebUI/Models/CountryPlayer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class CountryPlayer
+    {
+        public string Color { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string PlayerUsername { get; set; }
+    }
+}
